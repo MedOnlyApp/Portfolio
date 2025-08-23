0cef2f89394878257458f6431d4d8e03c8bac38c
Mohamed Rouane <131095232+MedOnlyApp@users.noreply.github.com>
2025-08-23 06:08:25 +0100

    Add files via upload

--- static/javascript/contact_panel.js
+++ static/javascript/contact_panel.js
@@ -1,9 +1,47 @@
 const contact_panel = document.getElementById("contact_id")
+const contact_type = document.querySelectorAll(".contact-type")
+const contact_button = document.querySelectorAll(".contact-button")
 
 contact_panel.addEventListener("mouseenter", (e) => {
-    contact_panel.style.width = "200px"
+    contact_panel.style.width = "150px"
+
+    contact_type.forEach(element => {
+        element.style.left = "40%"
+    })
 })
 
 contact_panel.addEventListener("mouseleave", (e) => {
     contact_panel.style.width = "50px"
+    
+    contact_type.forEach(element => {
+        element.style.left = "100%"
+    })
+})
+
+
+contact_button.forEach(element => {
+    element.addEventListener("click", (e) => {
+        contact_panel.style.width = "150px"
+        
+        contact_type.forEach(element => {
+            element.style.left = "40%"
+        })
+        // e.preventDefault()
+        // e.preventDefault()
+
+        // document.addEventListener('click', function(event) {
+        //     // contact_button.forEach(element => {
+        //         if (!contact_panel.contains(event.target)) {
+        //             console.log('Clicked outside the element!');
+        //             contact_panel.style.width = "50px"
+                    
+        
+        //             contact_type.forEach(element => {
+        //                 element.style.left = "100%"
+        //             })
+                    
+        //         }
+        //     // })
+        // })
+    })
 })
--- static/javascript/other.js
+++ static/javascript/other.js
@@ -4,12 +4,10 @@ window.addEventListener("scroll", () => {
     if (window.scrollY > 100) { // 500px from top
         button.style.bottom = "4%"
         button.style.opacity = "1";
-        // button.style.display = "inline-block";
     }
     else if (window.scrollY < 100) { // 500px from top
         button.style.bottom = "-10%"
         button.style.opacity = "0";
-        // button.style.display = "inline-block";
     }
 });
 
