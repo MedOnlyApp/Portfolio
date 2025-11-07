747ae1645884eccb50f06cf3a45c57fdad3613ad
Mohamed Rouane <131095232+MedOnlyApp@users.noreply.github.com>
2025-11-07 18:00:58 +0100

    Update welcome.js

--- static/javascript/welcome.js
+++ static/javascript/welcome.js
@@ -4,10 +4,8 @@ let random = 0
 
 stars.forEach(star => {
     random = Math.random() * 100;
-    console.log(random)
     star.style.setProperty('--random-left', `${Math.floor(random)}%`);
     random = Math.random() * 100;
     star.style.setProperty('--random-top', `${Math.floor(random)}%`);
 
-    console.log("zazk")
 });
