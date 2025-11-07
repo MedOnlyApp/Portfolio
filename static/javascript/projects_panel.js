18d7abd07e8bf6e022569139e4cf46e4b5fed71b
Mohamed Rouane <131095232+MedOnlyApp@users.noreply.github.com>
2025-11-07 18:00:29 +0100

    Update projects_panel.js

--- static/javascript/projects_panel.js
+++ static/javascript/projects_panel.js
@@ -1,7 +1,6 @@
 const project_panel = document.getElementById("project_body_id")
 const project_carts = document.querySelectorAll(".card-container")
 
-console.log("moh")
 
 project_carts.forEach(card => {
     card.addEventListener("mouseenter", () => {
@@ -9,17 +8,10 @@ project_carts.forEach(card => {
     })
     
     card.addEventListener("mouseleave", () => {
-        console.log("moh2")
         
         card.children[0].style.transform = "rotateY(0)"
     })
 })
-
-console.log("moh3")
-
-
-
-
 
 
 
